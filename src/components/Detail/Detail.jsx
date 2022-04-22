7790e5553ce078290eef328e13f20b9f5b72b232
Seba <sebastiandelescabe@gmail.com>
2022-04-21 22:20:36 -0300

    detalles terminados responsive

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'
-import { getInfoById, detailInfo } from '../../helpers';
-import { detailfooter1, detailfooter2 } from '../../assets';
+import { getInfoById } from '../../helpers';
 import './Detail.css';
 
 const Detail = () => {
@@ -27,41 +26,32 @@ const Detail = () => {
           <span className='detail__header-text-description' dangerouslySetInnerHTML={{ __html: carById.description }} />
         </div>
       </div>
-      <div className='detail__slideshow'>
+      <div className='detail__features'>
         {
-          carById.model_highlights && carById.model_highlights.map(e =>
+          carById.model_highlights && carById.model_features.map(e =>
           (
-            <div className='detail__slideshow-container'>
-              <img className='detail__slideshow-container-image' src={e.image} alt="not found" />
-              <span className='detail__slideshow-container-title'>{e.title}</span>
-              <span className='detail__slideshow-container-text' dangerouslySetInnerHTML={{ __html: e.content }} />
+            <div key={e.id} className='detail__features-container'>
+              <img className='detail__features-container-image' src={e.image} alt="not found" />
+              <span className='detail__features-container-title'>{e.name}</span>
+              <span className='detail__features-container-text' dangerouslySetInnerHTML={{ __html: e.description }} />
             </div>
           ))
         }
       </div>
-      <div className='detail__data'>
-        <div className='detail__data-one'>
-          <div className='detail__data-containerText'>
-            <span className='detail__data-containerText-title'>Titulo de 20px</span>
-            <span className='detail__data-containerText-text'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis hic voluptates itaque, velit deserunt enim assumenda culpa
-              libero,quos facere reprehenderit eligendi iste eaque vel delectus corrupti doloremque soluta consequatur!...</span>
-          </div>
-          <img className='detail__data-one-img' src={detailfooter2} alt="not found" />
-        </div>
-
-        <div className='detail__data-two'>
-          <img className='detail__data-two-img' src={''} alt="not found" />
-          <div className='detail__data-containerText'>
-            <span className='detail__data-containerText-title'>Titulo de 20px</span>
-            <span className='detail__data-containerText-text'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis hic voluptates itaque, velit deserunt enim assumenda culpa
-              libero, quos facere reprehenderit eligendi iste eaque vel delectus corrupti doloremque soluta consequatur!...
-            </span>
-          </div>
-        </div>
+      <div className='detail__higligths'>
+        {
+          carById.model_features && carById.model_highlights.map(e => (
+            <div key={e.id} className='detail__higligths-data'>
+              <img className='detail__higligths-img' src={e.image} alt="not found" />
+              <div className='detail__higligths-containerText'>
+                <span className='detail__higligths-containerText-title'>{e.title}</span>
+                <span className='detail__higligths-containerText-text' dangerouslySetInnerHTML={{ __html: e.content }}></span>
+              </div>
+            </div>
+          ))
+        }
       </div>
-    </div>
+    </div >
   )
 }
 
